5c28bb2f6c4ffa62a5895b7d5193b86ea15b72bb
Renee Osbourne <reneeosbourne@gmail.com>
2024-02-23 16:25:22 -0500

    Updates to yaml and index file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send("Company Name: Wild Rydes, Developer Name: Renee Osbourne, Company ID: 100939044");
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:3000`);
 });
