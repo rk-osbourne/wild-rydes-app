09cb87c50cfae928addff8bb066e1fb3ebc63266
Renee Osbourne <reneeosbourne@gmail.com>
2024-02-23 18:00:26 -0500

    Test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
  
 app.get('/', (req, res) => {
-  res.send("Company Name: Wild Rydes, Developer Name: Renee Osbourne, Company ID: 100939044.");
+  res.send("Company Name: Wild Rydes, Developer Name: Renee Osbourne, Company ID: 100939044");
 });
  
 app.listen(port, () => {
