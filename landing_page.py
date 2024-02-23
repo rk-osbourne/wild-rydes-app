c77eaca0e5bf3d4da9094b40f27559a071030774
Renee Osbourne <reneeosbourne@gmail.com>
2024-02-23 14:58:45 -0500

    Pivoting to node.js

--- landing_page.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Company Name: Wild Rydes, Developer Name: Renee Osbourne, Company ID: Your 100939044"
-
-if __name__ == '__main__':
-    app.run(debug=True)
