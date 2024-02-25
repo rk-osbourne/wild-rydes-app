a41832e4d7cbbcaa1847c0d2b8e749b6671458db
rk-osbourne <43052224+rk-osbourne@users.noreply.github.com>
2024-02-24 23:25:03 -0500

    Update wild_rydes_app.py

--- wild_rydes_app.py
+++ wild_rydes_app.py
@@ -1,9 +1,14 @@
 from flask import Flask
-app = Flask(_name_)
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Company Name: Wild Rydes, Developer Name: Renee Osbourne, Company ID: 100939044"
+    company_name = "Wild Rydes"
+    developer_name = "Renee Osbourne"
+    company_id = "100939044"
 
-if _name_ == '_main_':
-    app.run(debug=True)
+    return f"Company Name: {company_name}, Developer Name: {developer_name}, Company ID: {company_id}"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
